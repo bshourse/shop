e99d29f92d5d9207063333aea48d846f83681e8a
Dmitry <agapov.box@gmail.com>
2021-01-04 20:16:53 +0300

    Add functionality to read collections of products by implement product_collection class

--- shop.rb
+++ shop.rb
@@ -1,19 +1,35 @@
 require_relative 'product'
 require_relative 'film'
 require_relative 'book'
+require_relative 'product_collection'
 
-current_path = File.dirname(__FILE__ )
-film = Film.from_file(current_path + '/data/films/1.txt')
-book = Book.from_file(current_path + '/data/books/1.txt')
-puts film
-puts book
+# Создаем коллекцию продуктов, передавая методу класса from_dir путь к папке
+# с подпапками films и books. ProductCollection сам знает, как там внутри лежат
+# эти файлы и сам разбереться, как их оттуда считать.
+
+collection = ProductCollection.from_dir(File.dirname(__FILE__ ) + '/data')
+# Сортируем продукты по возрастанию цены с помощью метода sort! экземпляра
+# класса ProductCollection
+collection.sort!(by: :price, order: :asc)
+# Получаем массив продуктов методом to_a и выводим каждый на экран, передавая
+# его методу puts в качестве аргумента.
+collection.to_a.each do |product|
+  puts product
+end
+
+
+# current_path = File.dirname(__FILE__ )
+# film = Film.from_file(current_path + '/data/films/1.txt')
+# book = Book.from_file(current_path + '/data/books/1.txt')
+# puts film
+# puts book
 
 # Пытаемся вызвать метод from_file у класса Product и ловим ошибку
-begin
-  Product.from_file(current_path + '/data/films/1.txt')
-rescue NotImplementedError
-  puts 'Метод класса Product.from_file не реализован'
-end
+# begin
+#   Product.from_file(current_path + '/data/films/1.txt')
+# rescue NotImplementedError
+#   puts 'Метод класса Product.from_file не реализован'
+# end
 =begin
 products = []
 
